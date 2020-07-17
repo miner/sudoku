82a5c5952c0c6c2c1a0280c015df20d59cb97a0e
Steve Miner <steveminer@gmail.com>
2020-07-17 10:53:15 -0400

    Justin Kramer's port of Norvig's sudoku solver

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject com.velisco/sudoku "0.1.0-SNAPSHOT"
+  :description "Yet another Clojure version of Norvig's Sudoku solver"
+  :url "https://github.com/miner/sudoku"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.10.1"]])
+
+
+
---
+++ test/miner/test_sudoku.clj
@@ -0,0 +1,7 @@
+(ns miner.test-sudoku
+  (:require [clojure.test :refer :all]
+            [miner.sudoku :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
